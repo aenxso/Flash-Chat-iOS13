526da26195f168773bf6924cfbd8b78feafc230e
angel <xxangel.singh@gmail.com>
2020-11-30 05:56:33 -0800

    Complete tutorial code

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Angel Singh on 2020-11-27.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
